5fcb5767d18a786311713b55fa65694235c72563
Michael <Michael.ginalick@gmail.com>
2015-07-04 16:33:25 -0500

    migrations added

--- db/migrate/20150704051336_create_users.rb
+++ db/migrate/20150704051336_create_users.rb
@@ -2,7 +2,14 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
+      t.string :phone
+      t.string :password_digest
+
       t.timestamps
     end
+    add_index(:users, :id)
   end
+
+
+
 end
--- db/migrate/20150704051359_create_trains.rb
+++ db/migrate/20150704051359_create_trains.rb
@@ -2,7 +2,14 @@ class CreateTrains < ActiveRecord::Migration
   def change
     create_table :trains do |t|
 
-      t.timestamps
+    	t.datetime :time
+  		t.string :line
+  		t.string :stop
+  		t.integer :user_id
+  		t.string :days
+  		
+  		t.timestamps
     end
+    add_index(:trains, :user_id)
   end
 end
