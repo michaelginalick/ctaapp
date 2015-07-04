98a28fde75dfaeb2a8db99754f541daac6e339a6
Michael <Michael.ginalick@gmail.com>
2015-07-04 16:38:23 -0500

    models added

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,10 @@
 class Train < ActiveRecord::Base
+
+	belongs_to :user
+	validates_presence_of :time
+	validates_presence_of :line
+	validates_presence_of :stop
+	validates_presence_of :user_id
+	validates_presence_of :days
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :trains
+  
+  validates_uniqueness_of :phone
+  validates_presence_of :phone
 end
