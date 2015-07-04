0526c213cb16ef1123f47f951eb98c016f24e856
Michael <Michael.ginalick@gmail.com>
2015-07-04 16:59:33 -0500

    nested resources added

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   post '/session/login' => 'session#login'
   get '/session/logout' => 'session#logout', :as => 'logout'
 
+  resources :users do
+    resources :trains
+  end
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
