7eae860611959928dfafa6b7d382840ff52df647
Michael <Michael.ginalick@gmail.com>
2015-07-11 10:11:34 -0500

    cleaned up js and controller

--- app/assets/javascripts/trainstops.js
+++ app/assets/javascripts/trainstops.js
@@ -1,51 +1,34 @@
 $(document).ready(function(){
 
-		// Change train stops for each line selected
-		$(function() {
-    $('#select-line').change(function() {
-
-        var id = $(this).val();
-
-        $(".convenient").addClass("hidden");
-
-        $("#" + id).removeClass("hidden");
+	// Change train stops for each line selected
+	$(function() {
+        $('#select-line').change(function() {
+            var id = $(this).val();
+            $(".convenient").addClass("hidden");
+            $("#" + id).removeClass("hidden");
     	});
-		});
+	});
 
 
-		$('#new-stop').submit(function(event){
-
-			event.preventDefault();
-
-			if (!$("#select-time").val() || $("input:checkbox:checked").length == 0) {
-
-				 $("#stop-header").text("Please enter a time and date.")
-
+	$('#new-stop').submit(function(event){
+		event.preventDefault();
+        if (!$("#select-time").val() || $("input:checkbox:checked").length == 0) {
+            $("#stop-header").text("Please enter a time and date.")
             restoreStopHeader();
-      } else {
-      	var id = $("#user").val();
-
-      	$.ajax({
-      		      url: '/user/' + id + '/train/',
-               
+        } else {
+      	    var id = $("#user").val();
+      	     $.ajax({
+      		    url: '/user/' + id + '/train/',
                 method: 'POST',
-                
                 type: 'json',
-
                 data: {
-
-
-                	  time: $("#select-time").val(),
-                    
+                	time: $("#select-time").val(),
                     line: $("#select-line").val(),
-                    
                     // Workaround to dynamically match stop with line
                     stop: $("#select-stop-" + $("#select-line").val().toLowerCase()).val(),
-
                     days: {
 
-
-                    	  monday: $("#monday:checked").val(),
+                    	monday: $("#monday:checked").val(),
                         
                         tuesday: $("#tuesday:checked").val(),
                         
@@ -62,7 +45,7 @@ $(document).ready(function(){
                 },
                 success: function(data) {
 
-                	  $("#stop-header").text(data);
+                	$("#stop-header").text(data);
                     
                     $('#partials-div').load('/user/' + id + '/stop');
                     
@@ -89,10 +72,10 @@ $(document).ready(function(){
 
                     restoreStopHeader();
                 }
-      			});
+      		});
       	};
 
-		});
+	});
 
 	
 		    // Define functions
--- app/controllers/train_controller.rb
+++ app/controllers/train_controller.rb
@@ -4,50 +4,36 @@ class TrainController < ApplicationController
     @user = User.find(session[:user_id])
 
     if @user.trains.length < 50
-      day_string = ''
+       day_string = ''
 
-      params[:days].each_value do |value|
-        day_string += value
-      end
+        params[:days].each_value do |value|
+         day_string += value
+        end
 
-      @train = Train.create(stop: params[:stop], line: params[:line], user_id: params[:user_id], time: params[:time].to_time, days: day_string)
+      @train = Train.create(stop: params[:stop], 
+                            line: params[:line], 
+                            user_id: params[:user_id], 
+                            time: params[:time].to_time, 
+                            days: day_string)
 
       if Time.now > @train.time
-        @train.time += (60*60*24) #reschedule times from the past to the future so they are not run instantly
+        #reschedule times from the past to the future so they are not run instantly
+        @train.time += (60*60*24) 
         @train.save!
       end
-
       #SendTimes.perform_at(@train.time, @train.id)
-
-
       if request.xhr?
         render :json => @train
       end
-
-
-      "Stop added!"
-
-      #redirect_to(profile_path(@user))
-
     else
       redirect_to(profile_path(@user))
-      "Limit of three train stops exceeded!"
     end
 
   end
 
   def destroy
-    @user = User.find(session[:user_id])
     @stop = Train.find(params[:id])
     @stop.destroy
     redirect_to(profile_path)
   end
-
-
-
-
-
-
-
-
 end
