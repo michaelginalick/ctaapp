d69f842fc3d4a6b4fc0cabd0fea9f929ae08a66c
Michael <Michael.ginalick@gmail.com>
2015-07-12 21:43:47 -0500

    train controller refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,45 @@ class ApplicationController < ActionController::Base
 		return Twilio::REST::Client.new(account_sid, auth_token)
 	end
 
+	def train_stuff
+
+   		get_days
+
+      @train = Train.create(stop: params[:stop], 
+                            line: params[:line], 
+                            user_id: params[:user_id], 
+                            time: params[:time].to_time, 
+                            days: day_string)
+      #SendTimes.perform_at(@train.time, @train.id)
+      if request.xhr?
+        render :json => @train
+      end
+    end
+  end 
+
+  def get_days
+  	 if @user.trains.length < 50
+       day_string = ''
+
+        params[:days].each_value do |value|
+         day_string += value
+
+         return day_string
+
+        end
+      else
+      redirect_to(profile_path(@user))
+     end	
+  end
+
+  def set_time_save_train
+  	if Time.now > @train.time
+    	#reschedule times from the past to the future so they are not run instantly
+      @train.time += (60*60*24) 
+      @train.save!
+    end
+
+  end
+
+
 end
--- app/controllers/train_controller.rb
+++ app/controllers/train_controller.rb
@@ -3,32 +3,7 @@ class TrainController < ApplicationController
   def create
     @user = User.find(session[:user_id])
 
-    if @user.trains.length < 50
-       day_string = ''
-
-        params[:days].each_value do |value|
-         day_string += value
-        end
-
-      @train = Train.create(stop: params[:stop], 
-                            line: params[:line], 
-                            user_id: params[:user_id], 
-                            time: params[:time].to_time, 
-                            days: day_string)
-
-      if Time.now > @train.time
-        #reschedule times from the past to the future so they are not run instantly
-        @train.time += (60*60*24) 
-        @train.save!
-      end
-      #SendTimes.perform_at(@train.time, @train.id)
-      if request.xhr?
-        render :json => @train
-      end
-    else
-      redirect_to(profile_path(@user))
-    end
-
+    train_stuff
   end
 
   def destroy
